c9bbadd905aa7ffd76d326036c09bb3851acdb7d
offor <offorchukwuebuka8@gmail.com>
2024-03-15 19:20:28 +0100

    Your commit message here

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD4eAkgh7LQZ6brR0DYWBOnfVn_MQBSYvI",
-  authDomain: "redo-215cb.firebaseapp.com",
-  projectId: "redo-215cb",
-  storageBucket: "redo-215cb.appspot.com",
-  messagingSenderId: "490401602806",
-  appId: "1:490401602806:web:54c5bb8284bc31203260d7",
-  measurementId: "G-540R7XDBR9"
-};
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCNHxkNRv4dBQWlWaF8BWrzkfknzdni_rM",
+  authDomain: "hamuscreativity.firebaseapp.com",
+  projectId: "hamuscreativity",
+  storageBucket: "hamuscreativity.appspot.com",
+  messagingSenderId: "1076368551775",
+  appId: "1:1076368551775:web:36bc9c79399b784bf0d0ea",
+  measurementId: "G-FQRMTJ6145"
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);

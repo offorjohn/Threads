f90296e2abe335f30da79fd6c8dfc1f6665f4b1f
offor <offorchukwuebuka8@gmail.com>
2024-03-16 06:29:53 +0100

    Your commit message here

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,47 +1,39 @@
-import { Box, Flex, Container, Skeleton, SkeletonCircle, VStack } from '@chakra-ui/react'
-import FeedPost from './FeedPost'
-import { useEffect, useState } from 'react';
+import { Box, Container, Flex, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react";
+import FeedPost from "./FeedPost";
+import useGetFeedPosts from "../../hooks/useGetFeedPosts";
 
 const FeedPosts = () => {
+	const { isLoading, posts } = useGetFeedPosts();
 
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false)
-        }, 2000)
-    }, [])
-    return (
-        <Container maxW={"container.sm"} py={10} px={2}>
-            {isLoading && [0, 1, 2, 3].map((_, idx) => (
-                <VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
-                    <Flex gap='2'>
-                        <SkeletonCircle size='10' />
-                        <VStack gap={2} alignItems={"flex-start"}>
-                            <Skeleton height='10px' w={"200px"} />
-                            <Skeleton height='10px' w={"200px"} />
+	return (
+		<Container maxW={"container.sm"} py={10} px={2}>
+			{isLoading &&
+				[0, 1, 2].map((_, idx) => (
+					<VStack key={idx} gap={4} alignItems={"flex-start"} mb={10}>
+						<Flex gap='2'>
+							<SkeletonCircle size='10' />
+							<VStack gap={2} alignItems={"flex-start"}>
+								<Skeleton height='10px' w={"200px"} />
+								<Skeleton height='10px' w={"200px"} />
+							</VStack>
+						</Flex>
+						<Skeleton w={"full"}>
+							<Box h={"400px"}>contents wrapped</Box>
+						</Skeleton>
+					</VStack>
+				))}
 
-                        </VStack>
-                    </Flex>
-                    <Skeleton w={"full"}>
-                        <Box h={"500px"}>contents wrapped</Box>
-                    </Skeleton>
-                </VStack>
-            ))}
-            {!isLoading && (
-                <>
-                    <FeedPost img='/img1.png' username='burakorkmezz' avatar='/img1.png' />
-                    <FeedPost img='/img2.png' username='bur' avatar='/img2.png' />
+			{!isLoading && posts.length > 0 && posts.map((post) => <FeedPost key={post.id} post={post} />)}
+			{!isLoading && posts.length === 0 && (
+				<>
+					<Text fontSize={"md"} color={"red.400"}>
+						Dayuum. Looks like you don&apos;t have any friends.
+					</Text>
+					<Text color={"red.400"}>Stop coding and go make some!!</Text>
+				</>
+			)}
+		</Container>
+	);
+};
 
-                    <FeedPost img='/img3.png' username='Ella' avatar='/img3.png' />
-
-                    <FeedPost img='/img4.png' username='Ofor' avatar='/img4.png' />
-
-                </>
-            )}
-
-        </Container>
-
-    )
-}
-
-export default FeedPosts
+export default FeedPosts;
--- src/components/Modals/CommentsModal.jsx
+++ src/components/Modals/CommentsModal.jsx
@@ -10,7 +10,33 @@ import {
     ModalOverlay,
 } from "@chakra-ui/react";
 
-const CommentsModal = ({ isOpen, onClose }) => {
+import Comment from "../Comment/Comment";
+import usePostComment from "../../hooks/usePostComment";
+import { useEffect, useRef } from "react";
+
+const CommentsModal = ({ isOpen, onClose, post }) => {
+    const {handlePostComment, isCommenting}=usePostComment()
+    const commentRef = useRef(null);
+    const commentsContainerRef = useRef(null);
+
+    const handleSubmitComment = async (e) => {
+
+        e.preventDefault()
+        await handlePostComment(post.id, commentRef.current.value)
+        commentRef.current.value = ''
+
+    };
+    useEffect(() => {
+        const scrollToBottom = () => {
+            commentsContainerRef.current.scrollTop = commentsContainerRef.current.scrollHeight
+        }; 
+        if(isOpen){
+
+            setTimeout(() => {}, 100);
+        }
+
+
+    },[isOpen,post.comment.length])
     return (
         <Modal isOpen={isOpen} onClose={onClose} motionPreset='slideInLeft'>
             <ModalOverlay />
@@ -18,11 +44,23 @@ const CommentsModal = ({ isOpen, onClose }) => {
                 <ModalHeader>Comments</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody pb={6}>
-                    <Flex mb={4} gap={4} flexDir={"column"} maxH={"250px"} overflowY={"auto"}></Flex>
-                    <form style={{ marginTop: "2rem" }}>
-                        <Input placeholder='Comment' size={"sm"} />
+                    <Flex mb={4} gap={4} flexDir={"column"} maxH={"250px"} overflowY={"auto"}
+                    ref={commentsContainerRef}
+                    
+                    
+                    >
+
+
+                        {post.comments.map((comment, idx)=>
+                          <Comment key={idx} comment={comment} />
+                        
+                        )}
+                    </Flex>
+
+                    <form onSubmit={handleSubmitComment} style={{ marginTop: "2rem" }}>
+                        <Input placeholder='Comment' size={"sm"} ref={commentRef} />
                         <Flex w={"full"} justifyContent={"flex-end"}>
-                            <Button type='submit' ml={"auto"} size={"sm"} my={4}>
+                            <Button type='submit' ml={"auto"} size={"sm"} my={4} isLoading={isCommenting}>
                                 Post
                             </Button>
                         </Flex>

31018121963db3d01ef83c296f6cc446ddaa7336
offor <offorchukwuebuka8@gmail.com>
2024-03-17 21:43:54 +0100

    Your commit message here

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -6,7 +6,7 @@ import useGetUserProfileById from "../../hooks/useGetUserProfileById";
 const FeedPost = ({post}) => {
    const { userProfile } =useGetUserProfileById(post.createdBy);
    return (
-    <>
+    <>%
        <PostHeader post={post}  creatorProfile={userProfile}/> 
        <Box my={2} borderRadius={4} overflow={"hidden"}>
          <Image src={post.imageURL} alt={"FEED POST IMG"} />
